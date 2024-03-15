ba780ba60bd3627f307eee7d0c6958baf2023a85
Alex Straight <alex@Alexs-Laptop.local>
2024-03-15 01:45:27 -0700

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Header from "@/components/landing-page/header";
 import Title from "@/components/landing-page/title";
 
 export default async function Home() {
-  let data;
-  const response = await fetch("http://localhost:3000/api/google");
-  if (response.ok) {
-    data = await response.json();
-  } else {
-    console.log("something went wrong");
-    data = "something went wrong";
-  }
+  // let data;
+  // const response = await fetch("http://localhost:3000/api/google");
+  // if (response.ok) {
+  //   data = await response.json();
+  // } else {
+  //   console.log("something went wrong");
+  //   data = "something went wrong";
+  // }
 
   return (
     <main className="flex min-h-screen flex-col  items-center p-24">
