4f7513fa68e6630646b77960a88c716d3293367b
Alex Straight <alex@Alexs-Laptop.local>
2024-04-01 19:57:55 -0700

    removed suspense from layout:

--- app/(classes)/layout.tsx
+++ app/(classes)/layout.tsx
@@ -1,24 +1,10 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import { Suspense } from "react";
+import { Skeleton } from '@/components/ui/skeleton'
+import { Suspense } from 'react'
 
 export default function ClassLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return (
-    <div className="p-24">
-      <Suspense
-        fallback={
-          <div className="flex flex-col space-y-4">
-            {Array.from({ length: 5 }, (_, index) => (
-              <Skeleton className="h-4" key={index} />
-            ))}
-          </div>
-        }
-      >
-        {children}
-      </Suspense>
-    </div>
-  );
+  return <div className="p-24">{children}</div>
 }
