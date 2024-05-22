c306bac826fe676260b5663a41577fbe66a60369
profmanjupriya <mmuralidharanpriya@sdsu.edu>
2024-05-22 01:06:15 -0700

    more spacing issues fixed

--- components/landing-page/title.tsx
+++ components/landing-page/title.tsx
@@ -4,9 +4,13 @@ const Title = () => {
       <h1 className="sm:leading-loose font-roboto_condensed tracking-tight text-3xl md:text-5xl pt-24 md:pt-12 mb-5 text-center text-discordText">
         Manju's Data Structures & Algorithms
       </h1>
+      <p className="font-dm_serif_display text-center text-slate-600">
+        Welcome to the class knowledge hub .
+      </p>
       <p className="font-dm_serif_display text-center text-slate-600 mb-10">
-        Welcome to the class knowledge hub . Everything and anything you need
-        for my sections of CS210 and CS460 can be found within this website.
+        {' '}
+        Everything and anything you need for my sections of CS210 and CS460 can
+        be found within this website.
       </p>
     </div>
   )
